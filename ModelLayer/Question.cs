9e196be9938f5e02d72b92116633b0e67f8ae1b7
Sangey <Sangeylama@gmail.com>
2017-11-05 16:38:46 +0100

    Added Question model class

---
+++ ModelLayer/Question.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelLayer
+{
+    public class Question
+    {
+        public int Id { get; set; }
+        public string QuestionText { get; set; }
+        public string Difficulty { get; set; }
+
+    }
+}
