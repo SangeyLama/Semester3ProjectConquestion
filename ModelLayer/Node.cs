68c075b5b4999eccfa3e443dc6123194aabce87f
tamaskalapacs <kalapacs.tamas@gmail.com>
2017-11-06 19:33:42 +0100

    test

--- ModelLayer/Node.cs
+++ ModelLayer/Node.cs
@@ -8,7 +8,7 @@ namespace ModelLayer
 {
     class Node
     {
-        public int x { get; set; }
-        public int y { get; set; }
+        public int id { get; set; }
+        public List<Node> Neighbours { get; set; }
     }
 }
