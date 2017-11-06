6e12491cd46434677b84c13d15426ae61e0b2a18
tamaskalapacs <kalapacs.tamas@gmail.com>
2017-11-06 19:05:39 +0100

    git push
    git commit
    git push
    git

---
+++ ModelLayer/Node.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelLayer
+{
+    class Node
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+    }
+}
