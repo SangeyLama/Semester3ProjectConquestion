75d8fe3968b58ed04cf5d379b9f079eab4e466c5
MimiMicek <makaveli_girl1996@hotmail.com>
2017-11-07 10:46:16 +0100

    Added Difficulty class

---
+++ ModelLayer/Difficulty.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelLayer
+{
+    class Difficulty
+    {
+        public string EasyQuestions { get; set; }
+        public string HardQuestions { get; set; }
+    }
+}
