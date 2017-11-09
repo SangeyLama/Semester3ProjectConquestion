a51922586e9332ad7059a51adc2e72d3baffa47f
MadalinaKhaji <madalina.khaji10@gmail.com>
2017-11-09 13:25:44 +0100

    debug service error

---
+++ LogicLayer/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using DataLayer.DataLayer.Model;
+using DataLayer;
+
+namespace LogicLayer
+{
+    public class PlayerController
+    {
+        ConquestionDBContext db = new ConquestionDBContext();
+        public Player CreatePlayer(string name)
+        {
+            Player player = new Player(name);
+            db.Players.Add(player);
+            return player;
+        }
+    }
+}
