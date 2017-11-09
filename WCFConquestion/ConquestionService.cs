96a97a7e4a7c3eaf05fa67a17d290cd8c46a2e50
MadalinaKhaji <madalina.khaji10@gmail.com>
2017-11-09 13:25:44 +0100

    debug service error

---
+++ WCFConquestion/ConquestionService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using DataLayer.DataLayer.Model;
+
+using LogicLayer;
+using DataLayer;
+
+namespace WCFConquestion
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class ConquestionService : IConquestionService
+    {
+
+        public void CreatePlayer(string name)
+        {
+            PlayerController ctr = new PlayerController();
+            Player player = ctr.CreatePlayer(name);   
+        }
+    }
+}
