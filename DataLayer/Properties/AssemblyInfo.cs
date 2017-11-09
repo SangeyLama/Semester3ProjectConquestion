898aaa6b75b11c38937fcaad81091df28c5c1825
Sangey <sangeylama@gmail.com>
2017-11-09 10:54:18 +0100

    Setting up project scaffold

--- DataLayer/Properties/AssemblyInfo.cs
+++ DataLayer/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ModelLayer")]
+[assembly: AssemblyTitle("DataLayer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ModelLayer")]
+[assembly: AssemblyProduct("DataLayer")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("4799817d-63a4-4019-8bd7-5613cb53596a")]
+[assembly: Guid("e19ba2a4-91ed-46fb-ae91-5b1190ed4a9f")]
 
 // Version information for an assembly consists of the following four values:
 //
