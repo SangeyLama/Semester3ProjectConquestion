01bc8ca646cf91d8b029ae1f71255b1e306bf191
Sangey <sangeylama@gmail.com>
2017-11-15 11:07:40 +0100

    Fixing the last of the shit

---
+++ DataLayer/DataLayer.Model/Game.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.DataLayer.Model
+{
+    [DataContract]
+    public class Game
+    {
+        public int Id { get; set; }
+        [DataMember]
+        public String Name { get; set; }
+        [DataMember]
+        public List<Player> Players { get; set; }
+
+    }
+}
