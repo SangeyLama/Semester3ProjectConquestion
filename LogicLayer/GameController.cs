7b7d5400e9d82d58850a8c36caad143d226b7478
Sangey <sangeylama@gmail.com>
2017-11-15 11:42:52 +0100

    fixed tests and controllers

--- LogicLayer/GameController.cs
+++ LogicLayer/GameController.cs
@@ -24,17 +24,14 @@ namespace LogicLayer
         {
             game.Players = new List<Player>();
             game.Players.Add(player);
-            var gameEntity = db.Games.Where(g => g.Name == game.Name) .FirstOrDefault();
-            db.Entry(gameEntity).Collection(g => g.Players).Load();
-            if (gameEntity != null)
+            if (game != null)
             {
-                if (gameEntity.Players == null)
-                    gameEntity.Players = new List<Player>();
-                var playerEntity = db.Players.Where(p => p.Name == player.Name).FirstOrDefault();
-                if (!gameEntity.Players.Contains(playerEntity))
+                if (game.Players == null)
+                    game.Players = new List<Player>();
+                if (!game.Players.Contains((Player)player))
                 {
-                    gameEntity.Players.Add(playerEntity);
-                    db.Entry(gameEntity).State = System.Data.Entity.EntityState.Modified;
+                    game.Players.Add((Player)player);
+                    db.Entry(game).State = System.Data.Entity.EntityState.Modified;
                     db.SaveChanges();
                 }
 
